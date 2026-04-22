f36d8882aae7556c4f27aff1f4061f2d50416995
alexandretrotel <trotelalexandre@proton.me>
2026-04-22 09:16:56 -0400

    feat: add headers related code

---
+++ packages/fetch/src/headers.ts
@@ -0,0 +1,17 @@
+/**
+ * Merges two HeadersInit objects, with the second one taking precedence.
+ */
+export function mergeHeaders(base?: HeadersInit, override?: HeadersInit): Headers | undefined {
+  if (!(base || override)) {
+    return;
+  }
+
+  const merged = new Headers(base);
+  if (override) {
+    const overrideHeaders = new Headers(override);
+    for (const [key, value] of overrideHeaders.entries()) {
+      merged.set(key, value);
+    }
+  }
+  return merged;
+}
