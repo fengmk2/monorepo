291202f248a31e0af65d816bace016f02ad9a37a
alexandretrotel <trotelalexandre@proton.me>
2026-05-05 19:57:04 -0400

    chore: remove old vite.config.ts files

--- packages/fetch/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite-plus";
-
-export default defineConfig({
-  pack: {
-    dts: true,
-    entry: ["src/**/*", "!**/*.test.ts", "!**/*.spec.ts"],
-    exports: true,
-  },
-});
