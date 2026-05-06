d8b23a1fb82507009ebbf2132b14956c90e2cefa
alexandretrotel <trotelalexandre@proton.me>
2026-05-05 20:33:32 -0400

    fix: rework vitest config to separate node and browser tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+import { playwright } from "@vitest/browser-playwright";
 import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
@@ -7,11 +8,38 @@ export default defineConfig({
       reporter: ["html", "json", "lcov", "text"],
       exclude: [...configDefaults.exclude, "**/dist/**", "**/sequence-policy.ts"],
     },
-    environment: "node",
-    exclude: ["dist", "node_modules"],
+    exclude: [...configDefaults.exclude, "**/dist/**", "**/node_modules/**"],
     globals: true,
     outputFile: process.env.CI ? { junit: "./coverage/junit.xml" } : undefined,
-    projects: ["packages/*"],
+    projects: [
+      {
+        extends: true,
+        test: {
+          environment: "node",
+          include: ["packages/**/*.node.test.ts"],
+          name: {
+            color: "green",
+            label: "node",
+          },
+        },
+      },
+      {
+        extends: true,
+        test: {
+          browser: {
+            enabled: true,
+            headless: true,
+            instances: [{ browser: "chromium" }],
+            provider: playwright(),
+          },
+          include: ["packages/**/*.browser.test.ts"],
+          name: {
+            color: "cyan",
+            label: "browser",
+          },
+        },
+      },
+    ],
     reporters: process.env.CI ? ["dot", "junit"] : ["default"],
     restoreMocks: true,
   },
